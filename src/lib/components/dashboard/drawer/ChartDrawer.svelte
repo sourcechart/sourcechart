52345119e1c83b294b5432327623f3e8aa0319e7
tcollect <tccoleman26@gmail.com>
2023-06-08 09:35:39 -0400

    this works

--- src/lib/components/dashboard/drawer/ChartDrawer.svelte
+++ src/lib/components/dashboard/drawer/ChartDrawer.svelte
@@ -3,6 +3,7 @@
 	import AxisDropDrown from './dropdown/ColumnDropDrown.svelte';
 	import Groupby from './dropdown/Groupby.svelte';
 	import Aggregator from './dropdown/Aggregator.svelte';
+	import SideBar from './components/SideBar.svelte';
 	import { activeChart, clearChartOptions } from '$lib/io/stores';
 	import { clickOutside, clickInside } from '$lib/actions/clickUtils';
 	import { Drawer, Sidebar, SidebarWrapper } from 'flowbite-svelte';
@@ -47,36 +48,6 @@
 		drawerToggle = true;
 	}
 </script>
-
-<div
-	use:clickInside
-	on:click_inside={cInside}
-	use:clickOutside
-	on:click_outside={cOutside}
-	class="flex absolute container justify-center"
->
-	<Drawer transitionType="fly" {transitionParams} bind:hidden={$activeChart} id="sidebar2">
-		<Sidebar>
-			<SidebarWrapper divClass="overflow-y-auto py-4 px-3 rounded dark:bg-gray-800">
-				<DatasetDropDown />
-				<AxisDropDrown axis={'X'} />
-				<AxisDropDrown axis={'Y'} />
-
-				<div
-					class="text-center font-medium focus:ring-4 focus:outline-none inline-flex items-center justify-center px-5 py-2.5 text-sm text-gray-900 bg-white border border-gray-200 dark:border-gray-600 hover:bg-gray-100 focus:ring-gray-200 dark:bg-gray-800 dark:text-gray-400 hover:text-blue-700 focus:text-blue-700 dark:focus:text-white dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:bg-transparent dark:border-gray-800 dark:hover:border-gray-700 rounded-lg"
-				>
-					Choose Y Axis Aggregator
-				</div>
-				<Aggregator />
-				<Groupby />
-				<button
-					on:click={clearUpdatedChartObject}
-					type="button"
-					class="py-2.5 px-5 mr-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
-				>
-					Clear Choices</button
-				>
-			</SidebarWrapper>
-		</Sidebar>
-	</Drawer>
-</div>
+{#if drawerToggle}
+<Sidebar />
+{/if}
---
+++ src/lib/components/dashboard/drawer/components/SideBar.svelte
@@ -0,0 +1,48 @@
+<script lang='ts'>
+    import { onMount } from 'svelte';
+  
+    let isShown:boolean = false;
+  
+    const toggleSidebar = () => {
+      isShown = !isShown;
+    };
+  
+    onMount(() => {
+      document.addEventListener('click', toggleSidebar);
+      return () => {
+        document.removeEventListener('click', toggleSidebar);
+      };
+    });
+  </script>
+  
+  <style>
+    .sidebar {
+      @apply absolute bg-gray-700 text-white w-64 h-full overflow-auto transition-transform duration-200 ease-in-out;
+    }
+    .sidebar.hidden {
+      @apply -translate-x-full;
+    }
+  </style>
+  
+  <div class="sidebar" class:sidebar.hidden={!isShown}>
+    <div class="p-6">
+      <h1 class="text-2xl mb-4">My Sidebar</h1>
+      <div class="mb-2">
+        <h2 class="text-xl">Section 1</h2>
+        <ul class="pl-2">
+          <li>Item 1</li>
+          <li>Item 2</li>
+        </ul>
+      </div>
+      <div class="mb-2">
+        <h2 class="text-xl">Section 2</h2>
+        <ul class="pl-2">
+          <li>Item 3</li>
+          <li>Item 4</li>
+        </ul>
+      </div>
+    </div>
+  </div>
+  
+  <button on:click={toggleSidebar}>Toggle Sidebar</button>
+  
