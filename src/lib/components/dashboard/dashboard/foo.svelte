15618fff3450bf6905eb036fbd7c0b89c257b81e
tcollect <tccoleman26@gmail.com>
2023-06-08 10:53:00 -0400

    changing filename

--- src/lib/components/dashboard/dashboard/foo.svelte
+++ src/lib/components/dashboard/dashboard/foo.svelte
@@ -1,12 +1,10 @@
 <script>
 	import MoveableChart from '$lib/components/dashboard/charts/MoveableChart.svelte';
 	import { allCharts } from '$lib/io/stores';
-	import SideBar from '$lib/components/dashboard/drawer/components/SideBar.svelte';
-
 	import ChartDrawer from '$lib/components/dashboard/drawer/ChartDrawer.svelte';
 </script>
 
 {#each $allCharts as item (item)}
 	<MoveableChart id={item.chartID} chartOptions={item.chartOptions} />
 {/each}
-<SideBar />
+<ChartDrawer />
