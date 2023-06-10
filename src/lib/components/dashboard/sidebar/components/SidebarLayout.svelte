7507b58f9a8b1dba878bd8fea2b4c87b86dd62a5
tcollect <tccoleman26@gmail.com>
2023-06-10 19:22:08 -0400

    restructuring folder

---
+++ src/lib/components/dashboard/sidebar/components/SidebarLayout.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	export let open: boolean;
+	export let id: string = '';
+</script>
+
+<div
+	class={open
+		? 'bg-gray-700 text-white w-64 overflow-auto transition-transform duration-200 ease-in-out'
+		: '-translate-x-full invisible'}
+	{id}
+>
+	<div class="p-6">
+		<slot />
+	</div>
+</div>
