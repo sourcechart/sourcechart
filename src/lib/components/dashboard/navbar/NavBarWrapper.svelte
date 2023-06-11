35a641fc56788a4198db2b58b953c80696de6196
tcollect <tccoleman26@gmail.com>
2023-06-11 12:26:23 -0400

    navbar is rounded

--- src/lib/components/dashboard/navbar/NavBarWrapper.svelte
+++ src/lib/components/dashboard/navbar/NavBarWrapper.svelte
@@ -1,7 +1,7 @@
 <div
-	class="rounded-lg fixed inset-x-0 top-0 z-50 flex h-16 items-center justify-center bg-gray-700 shadow-lg"
+	class="rounded-md fixed inset-x-0 top-0 z-50 flex h-16 items-center justify-center bg-gray-800 shadow-lg"
 >
-	<div class="flex items-center justify-between">
+	<div class="flex items-center justify-between rounded-md">
 		<!-- Add your navbar content here. You can use NavUl, NavLi components as shown in the previous examples -->
 		<slot />
 	</div>
