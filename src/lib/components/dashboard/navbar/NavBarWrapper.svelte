5d3223b71c38f78d1041f087cc4171d7ace8162a
tcollect <tccoleman26@gmail.com>
2023-06-11 11:35:01 -0400

    moving to custom navbar

---
+++ src/lib/components/dashboard/navbar/NavBarWrapper.svelte
@@ -0,0 +1,6 @@
+<div class="fixed inset-x-0 top-0 z-50 flex h-16 items-center justify-center bg-gray-700 shadow-lg">
+	<div class="flex items-center justify-between">
+		<!-- Add your navbar content here. You can use NavUl, NavLi components as shown in the previous examples -->
+		<slot />
+	</div>
+</div>
