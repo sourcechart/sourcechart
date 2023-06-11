00c733f6130a38fbc643ebffa2e1aa52affcf4f0
tcollect <tccoleman26@gmail.com>
2023-06-11 11:35:01 -0400

    moving to custom navbar

--- src/lib/components/dashboard/navbar/NavBar.svelte
+++ src/lib/components/dashboard/navbar/NavBar.svelte
@@ -24,9 +24,7 @@
 		<div
 			class="flex items-center justify-between mx-1 rounded-md overflow-hidden dark:hover:bg-gray-600"
 		>
-			<div
-				class="relative flex align-center justify-items-center items-center flex-col dark:hover:bg-gray-600"
-			>
+			<div>
 				<svelte:component this={component.component} />
 			</div>
 		</div>
