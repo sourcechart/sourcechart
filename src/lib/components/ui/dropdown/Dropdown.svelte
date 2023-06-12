b8280e0e4cb34df576889437f32def0d0e97676d
tcollect <tccoleman26@gmail.com>
2023-06-12 17:03:56 -0400

    removing dropdown

--- src/lib/components/ui/dropdown/Dropdown.svelte
+++ src/lib/components/ui/dropdown/Dropdown.svelte
@@ -1,29 +1,23 @@
 <!-- src/lib/DropDown.svelte -->
-<script lang='ts'>
-    let show:boolean = false;
-    
-    const toggleDropdown = () => {
-        show = !show;
-    }
+<script lang="ts">
+	let show: boolean = false;
+
+	const toggleDropdown = () => {
+		show = !show;
+	};
 </script>
 
 <div class="dropdown h-4 w-auto">
-    <button on:click={toggleDropdown}>
-        <slot name="trigger"></slot>
-    </button>
+	<button on:click={toggleDropdown}>
+		<slot name="trigger" />
+	</button>
 
-    {#if show}
-        <div class="dropdown-menu">
-            <slot name="items"></slot>
-        </div>
-    {/if}
+	{#if show}
+		<div class="dropdown-menu">
+			<slot name="items" />
+		</div>
+	{/if}
 </div>
 
 <style>
-    .dropdown {
-        /* Your styling here */
-    }
-    .dropdown-menu {
-        /* Your styling here */
-    }
 </style>
