474aa35c6edb8aa0eb08bba14420bf9fe78f9782
tcollect <tccoleman26@gmail.com>
2023-06-11 22:37:49 -0400

    update Dark Mode

---
+++ src/lib/components/common/ui/DarkMode.svelte
@@ -0,0 +1,29 @@
+<script lang='ts'>
+    import { onMount } from 'svelte';
+  
+    let darkMode = false;
+  
+    onMount(() => {
+      // Get theme from local storage
+      const storedTheme = localStorage.getItem('theme');
+      if (storedTheme) {
+        darkMode = storedTheme === 'dark';
+      } else {
+        // Or set based on system preference
+        darkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
+      }
+  
+      // Apply the theme
+      document.body.classList = darkMode ? 'dark' : 'light';
+    });
+  
+    function toggleDarkMode() {
+      darkMode = !darkMode;
+      localStorage.setItem('theme', darkMode ? 'dark' : 'light');
+      document.body.classList = darkMode ? 'dark' : 'light';
+    }
+  </script>
+  
+  <button on:click={toggleDarkMode}>
+    Toggle Dark Mode
+  </button>
--- src/lib/components/common/ui/index.ts
+++ src/lib/components/common/ui/index.ts
@@ -1,5 +1,6 @@
 import Dropdown from './Dropdown.svelte'
 import DropdownItem from './DropdownItem.svelte'
 import SidebarWrapper from './SidebarWrapper.svelte'
+import  DarkMode  from './DarkMode.svelte'
 
-export {Dropdown, DropdownItem, SidebarWrapper}
+export {Dropdown, DropdownItem, SidebarWrapper, DarkMode}
