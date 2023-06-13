6ba982a2353493519ecbc8996062d3733770970a
tcollect <tccoleman26@gmail.com>
2023-06-13 14:05:57 -0400

    Dropdownbutton

--- src/lib/components/ui/dropdown/DropDownButton.svelte
+++ src/lib/components/ui/dropdown/DropDownButton.svelte
@@ -11,4 +11,19 @@
 	};
 </script>
 
-<button on:click={handleClick}> <slot /> </button>
+<button
+	id="dropdownDefaultButton"
+	data-dropdown-toggle="dropdown"
+	class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+	type="button"
+	on:click={handleClick}
+	><slot />
+	<svg
+		class="w-4 h-4 ml-2"
+		aria-hidden="true"
+		fill="none"
+		stroke="currentColor"
+		viewBox="0 0 24 24"
+		xmlns="http://www.w3.org/2000/svg"
+	/>
+</button>
