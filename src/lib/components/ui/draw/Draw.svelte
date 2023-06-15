36f97c130bce73b12af6f4953577b98ff7c269f2
tcollect <tccoleman26@gmail.com>
2023-06-15 15:06:15 -0400

    draw rectangles persist

--- src/lib/components/ui/draw/Draw.svelte
+++ src/lib/components/ui/draw/Draw.svelte
@@ -1,73 +1,84 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
 
-	export let width: number = 1000;
-	export let height: number = 1000;
+	interface Rect {
+		x: number;
+		y: number;
+		width: number;
+		height: number;
+	}
 
+	interface Point {
+		x: number;
+		y: number;
+	}
+
+	interface MouseEventExtended extends MouseEvent {
+		offsetX: number;
+		offsetY: number;
+	}
+
+	let width: number;
+	let height: number;
 	let canvas: HTMLCanvasElement;
 	let context: CanvasRenderingContext2D | null;
 	let isDrawing: boolean = false;
-	let start: { x: number; y: number };
-
-	let t: number, l: number;
+	let start: Point;
+	let rectangles: Rect[] = [];
 
 	onMount(() => {
 		context = canvas.getContext('2d');
-		handleSize();
+		width = window.innerWidth;
+		height = window.innerHeight;
 	});
 
-	const handleStart = ({ offsetX: x, offsetY: y }: { offsetX: number; offsetY: number }) => {
-		isDrawing = true;
-		start = { x, y };
-	};
-
-	const handleEnd = (): void => {
-		isDrawing = false;
-		if (context) context.clearRect(0, 0, width, height); // Clear the canvas
-	};
-
-	const handleMove = ({ offsetX: x1, offsetY: y1 }: { offsetX: number; offsetY: number }) => {
-		if (!isDrawing) return;
-
-		const { x, y } = start;
+	const drawRect = (rect: Rect): void => {
 		if (context) {
-			context.clearRect(0, 0, width, height); // Clear the canvas before drawing
 			context.beginPath();
-			context.rect(x, y, x1 - x, y1 - y);
+			context.rect(rect.x, rect.y, rect.width, rect.height);
 			context.stroke();
 		}
 	};
 
-	const handleSize = (): void => {
-		const { top, left } = canvas.getBoundingClientRect();
-		t = top;
-		l = left;
+	const handleStart = ({ offsetX: x, offsetY: y }: MouseEventExtended) => {
+		isDrawing = true;
+		start = { x, y };
+	};
+
+	const handleEnd = ({ offsetX: x, offsetY: y }: MouseEventExtended) => {
+		isDrawing = false;
+		rectangles.push({
+			x: start.x,
+			y: start.y,
+			width: x - start.x,
+			height: y - start.y
+		});
+	};
+
+	const handleMove = ({ offsetX: x, offsetY: y }: MouseEventExtended) => {
+		if (!isDrawing) return;
+		if (context) {
+			context.clearRect(0, 0, width, height); // Clear the canvas before drawing
+			rectangles.forEach(drawRect); // Draw all the completed rectangles
+			drawRect({ x: start.x, y: start.y, width: x - start.x, height: y - start.y }); // Draw current rectangle
+		}
 	};
 </script>
 
-<svelte:window on:resize={handleSize} />
+<svelte:window
+	on:resize={() => {
+		if (typeof window !== 'undefined') {
+			width = window.innerWidth;
+			height = window.innerHeight;
+		}
+	}}
+/>
 
 <canvas
 	{width}
 	{height}
 	bind:this={canvas}
 	on:mousedown={handleStart}
-	on:touchstart={(e) => {
-		const { clientX, clientY } = e.touches[0];
-		handleStart({
-			offsetX: clientX - l,
-			offsetY: clientY - t
-		});
-	}}
 	on:mouseup={handleEnd}
-	on:touchend={handleEnd}
-	on:mouseleave={handleEnd}
 	on:mousemove={handleMove}
-	on:touchmove={(e) => {
-		const { clientX, clientY } = e.touches[0];
-		handleMove({
-			offsetX: clientX - l,
-			offsetY: clientY - t
-		});
-	}}
 />
