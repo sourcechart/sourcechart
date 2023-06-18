04d8263652cb5104653f27819ce8d2346e6eec57
tcollect <tccoleman26@gmail.com>
2023-06-17 22:12:11 -0400

    moving navbar wrapper

--- src/lib/components/dashboard/navbar/NavBar.svelte
+++ src/lib/components/dashboard/navbar/NavBar.svelte
@@ -1,16 +1,20 @@
 <script lang="ts">
-	import { Cursor, Draw } from './navbar-icons';
-	import NavBarWrapper from './NavBarWrapper.svelte';
+	import { Cursor, Draw, Eraser } from './navbar-icons';
+	import NavBarWrapper from '$lib/components/ui/navbar/NavBarWrapper.svelte';
 	import { navBarMode } from '$lib/io/stores';
 
-	let components = [
+	let icons = [
 		{
-			name: 'CursorIcon',
+			name: 'Cursor',
 			component: Cursor
 		},
 		{
-			name: 'DrawBoundary',
+			name: 'Draw',
 			component: Draw
+		},
+		{
+			name: 'Eraser',
+			component: Eraser
 		}
 	];
 
@@ -20,7 +24,7 @@
 </script>
 
 <NavBarWrapper>
-	{#each components as { name, component } (name)}
+	{#each icons as { name, component } (name)}
 		<div class="flex items-center justify-between mx-1 rounded-md overflow-hidden">
 			<div class="relative flex align-center justify-items-center items-center flex-col">
 				<svelte:component this={component} on:mode={setMode} />
