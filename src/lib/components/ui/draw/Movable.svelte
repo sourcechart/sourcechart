e811566d9439a38639c8de5484af392fd635e906
tcollect <tccoleman26@gmail.com>
2023-06-17 21:40:18 -0400

    now it is all colerful

--- src/lib/components/ui/draw/Movable.svelte
+++ src/lib/components/ui/draw/Movable.svelte
@@ -65,6 +65,34 @@
 		}
 	};
 
+	const drawHandles = (polygon: Polygon) => {
+		if (context) {
+			// Create array to hold the positions of the handles
+			let handlePositions: Point[] = [...polygon.vertices];
+
+			// Calculate and add midway points between corners to the handlePositions
+			for (let i = 0; i < polygon.vertices.length; i++) {
+				let nextIndex = (i + 1) % polygon.vertices.length; // Ensures that the last point connects to the first
+				let midPoint: Point = {
+					x: (polygon.vertices[i].x + polygon.vertices[nextIndex].x) / 2,
+					y: (polygon.vertices[i].y + polygon.vertices[nextIndex].y) / 2
+				};
+				handlePositions.push(midPoint);
+			}
+
+			// Draw the outline and circles
+			context.strokeStyle = 'lime'; // Change this to your preferred color
+			context.lineWidth = 2; // Change this to your preferred line width
+			handlePositions.forEach((point) => {
+				if (context) {
+					context.beginPath();
+					context.arc(point.x, point.y, 5, 0, 2 * Math.PI); // Change the 3rd argument to adjust the size of the circle
+					context.stroke();
+				}
+			});
+		}
+	};
+
 	const getEdgeHovered = (polygon: Polygon): Edge | null => {
 		const { x, y } = currentMousePosition;
 
@@ -116,6 +144,11 @@
 			context.closePath();
 			context.strokeStyle = selectedPolygonIndex === index ? 'red' : 'black';
 			context.stroke();
+
+			// If this is the selected polygon, draw the handles
+			if (selectedPolygonIndex === index) {
+				drawHandles(polygon);
+			}
 		}
 	};
 
