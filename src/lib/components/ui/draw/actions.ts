3de45993621d42628ec21e27b19bd062f85759fc
tcollect <tccoleman26@gmail.com>
2023-06-19 14:07:03 -0400

    refactoring types

---
+++ src/lib/components/ui/draw/actions.ts
@@ -0,0 +1,26 @@
+export function mouseTracker(node: Node, {onMove}: {onMove: (x: number, y: number) => void}) {
+    const handleMouseMove = (event) =>{
+        onMove(event.clientX, event.clientY)
+    }
+
+    node.addEventListener("mousemove", handleMouseMove);
+    return {
+        destroy() {
+            node.removeEventListener("mousemove", handleMouseMove);
+        }
+    };
+}
+
+
+
+export function mouseClick(node:Node, {onClick}: {onClick:(x:number, y:number) => void} ) {
+    const handleClick = (event)  => {
+        onClick(event.clientX, event.clientY)
+    }
+    node.addEventListener("mousemove", handleClick);
+return {
+    destroy() {
+        node.removeEventListener("click", handleClick)
+       }
+    }
+}
