e4079c1cd7b1a4cc59c9bf0de13e369f15df0867
tcollect <tccoleman26@gmail.com>
2023-06-19 20:07:01 -0400

    updaitng file directory

--- src/lib/components/dashboard/charts/MoveableChart.svelte
+++ src/lib/components/dashboard/charts/MoveableChart.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import Moveable from 'svelte-moveable';
 	import EChart from './eChart.svelte';
-	import { clickEscapeKey } from '$lib/actions/clickUtils';
+	import { clickEscapeKey } from '$lib/actions/clickActions';
 	import { mostRecentChartID, allCharts, activeSidebar, clearChartOptions } from '$lib/io/stores';
 
 	export let chartOptions: object;
