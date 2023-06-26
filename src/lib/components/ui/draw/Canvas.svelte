7c6d32569ce673a15a39902e23d7512ee0e79eaf
tcollect <tccoleman26@gmail.com>
2023-06-25 21:38:30 -0400

    WIP

--- src/lib/components/ui/draw/Canvas.svelte
+++ src/lib/components/ui/draw/Canvas.svelte
@@ -1,22 +1,6 @@
 <script lang="ts">
-	import { Stage, Layer, Rect } from 'svelte-konva';
-
-	let width: number;
-	let height: number;
-	function handleClick(e) {
-		const konvaEvent = e.detail;
-		alert(`Clicked on rectangle: ${konvaEvent.type}`);
-	}
 </script>
 
-<Stage config={{ width: width, height: height }}>
-	<Layer>
-		<Rect
-			config={{ x: 100, y: 100, width: 400, height: 200,  stroke='red' }}
-			on:pointerclick={handleClick}
-		/>
-	</Layer>
-</Stage>
 <svelte:window
 	on:resize={() => {
 		if (typeof window !== undefined) {
