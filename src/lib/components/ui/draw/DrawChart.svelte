861b0ece4b826ce67124d3a9b39c68d97e42125e
tcollect <tccoleman26@gmail.com>
2023-06-25 21:38:30 -0400

    WIP

---
+++ src/lib/components/ui/draw/DrawChart.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import { Layer } from 'svelte-konva';
+
+	export let id: string;
+	export let x: number;
+	export let y: number;
+	let width: number;
+	let height: number;
+</script>
+
+<Layer>
+	<div {id}>
+		<!--From here, we are going to loop in the canvas-->
+		<slot />
+	</div>
+</Layer>
