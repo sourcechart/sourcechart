a7d85911e516f41d50a0ef86df8594f38206d664
tcollect <tccoleman26@gmail.com>
2023-06-27 11:41:56 -0400

    Draw

--- src/lib/components/ui/draw/Draw.svelte
+++ src/lib/components/ui/draw/Draw.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import { Stage, Layer, Rect } from 'svelte-konva';
+	import { Stage, Layer, Rect, Transformer } from 'svelte-konva';
 	import { writable } from 'svelte/store';
 
 	let width: number;
@@ -7,15 +7,28 @@
 	let fill: string = 'transparent';
 	let stroke: string = 'black';
 
+	type Rectangle = {
+		x: number;
+		y: number;
+		width: number;
+		height: number;
+		fill: string;
+		stroke: string;
+		draggable: boolean;
+	};
+
 	let rectangles = writable<Rectangle[]>([]);
 	let newRectangle: Rectangle[] = [];
 
+	let transformer;
+	let selectedShapeName = '';
+
 	const handleMouseDown = (e: Event & { detail: { target: any } }) => {
-		stroke = 'black';
-		if (newRectangle.length === 0) {
-			const { x, y } = e.detail.target.getStage().getPointerPosition();
-			newRectangle = [{ x, y, width: 0, height: 0, fill: fill, stroke: stroke, draggable: true }];
-		}
+		const konvaEvent = e.detail;
+
+		console.log(konvaEvent.target.scaleX());
+		const { x, y } = konvaEvent.target.getStage().getPointerPosition();
+		newRectangle = [{ x, y, width: 0, height: 0, fill: fill, stroke: stroke, draggable: true }];
 	};
 
 	const handleMouseUp = (e: Event & { detail: { target: any } }) => {
@@ -37,16 +50,7 @@
 			rectangles.update((oldRectangles) => [...oldRectangles, rectangleToAdd]);
 			newRectangle = [];
 		}
-		stroke = 'red';
 	};
-
-	const handleClick = (e: Event) => {
-		stroke = 'red';
-	};
-
-	const transformStart = (e: Event) => {};
-
-	const transformEnd = (e: Event) => {};
 
 	const handleMouseMove = (e: Event & { detail: { target: any } }) => {
 		if (newRectangle.length === 1) {
@@ -67,6 +71,10 @@
 			];
 		}
 	};
+
+	const foo = () => {
+		console.log('foo');
+	};
 </script>
 
 <Stage
@@ -77,17 +85,18 @@
 >
 	<Layer>
 		{#each $rectangles as rectangle}
-			<Rect config={rectangle} />
+			<Rect config={rectangle} on:transform={foo} />
 		{/each}
 		{#each newRectangle as rectangle}
-			<Rect config={rectangle} on:click={handleClick} />
+			<Rect config={rectangle} on:transform={foo} on:transformend={foo} />
 		{/each}
+		<Transformer bind:this={transformer} />
 	</Layer>
 </Stage>
 
 <svelte:window
 	on:resize={() => {
-		if (typeof window !== undefined) {
+		if (typeof window !== 'undefined') {
 			width = window.innerWidth;
 			height = window.innerHeight;
 		}
