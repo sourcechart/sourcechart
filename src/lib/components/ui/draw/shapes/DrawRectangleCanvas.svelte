042b80332872a6531a49fbd0628dd17e129b083f
tcollect <tccoleman26@gmail.com>
2023-06-30 10:25:31 -0400

    can resize most of the time

--- src/lib/components/ui/draw/shapes/DrawRectangleCanvas.svelte
+++ src/lib/components/ui/draw/shapes/DrawRectangleCanvas.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import { drawHandles, drawRectangle } from './draw-utils/Draw';
-	import { mouseEventState, navBarState } from '$lib/io/Stores';
+	import { mouseEventState, navBarState, polygons } from '$lib/io/Stores';
 	import { isPointInPolygon } from './draw-utils/PolygonOperations';
 	import { afterUpdate } from 'svelte';
 	import Handle from './Handle.svelte';
@@ -129,7 +129,46 @@
 	});
 
 	const handleDrag = (dx: number, dy: number, corner: string) => {
-		console.log(`Dragging ${corner} by dx=${dx}, dy=${dy}`);
+		if (corner === 'tl') {
+			polygon.vertices[0].x += dx;
+			polygon.vertices[0].y += dy;
+			polygon.vertices[1].y += dy;
+			polygon.vertices[3].x += dx;
+		}
+		if (corner === 'tr') {
+			polygon.vertices[1].x += dx;
+			polygon.vertices[1].y += dy;
+			polygon.vertices[0].y += dy;
+			polygon.vertices[2].x += dx;
+		}
+		if (corner === 'br') {
+			polygon.vertices[2].x += dx;
+			polygon.vertices[2].y += dy;
+			polygon.vertices[3].y += dy;
+			polygon.vertices[1].x += dx;
+		}
+		if (corner === 'bl') {
+			polygon.vertices[3].x += dx;
+			polygon.vertices[3].y += dy;
+			polygon.vertices[2].y += dy;
+			polygon.vertices[0].x += dx;
+		}
+		if (corner === 'mt') {
+			polygon.vertices[0].y += dy;
+			polygon.vertices[1].y += dy;
+		}
+		if (corner === 'mr') {
+			polygon.vertices[1].x += dx;
+			polygon.vertices[2].x += dx;
+		}
+		if (corner === 'mb') {
+			polygon.vertices[2].y += dy;
+			polygon.vertices[3].y += dy;
+		}
+		if (corner === 'ml') {
+			polygon.vertices[0].x += dx;
+			polygon.vertices[3].x += dx;
+		}
 	};
 </script>
 
