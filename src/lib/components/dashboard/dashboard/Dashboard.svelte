d46892357b917d3439816f88fb831bd714933195
tcollect <tccoleman26@gmail.com>
2023-07-01 12:17:46 -0400

    drawing circle

--- src/lib/components/dashboard/dashboard/Dashboard.svelte
+++ src/lib/components/dashboard/dashboard/Dashboard.svelte
@@ -1,16 +1,22 @@
 <!-- I only put this here so it would be easier to navigate to in the file tree. Could definitely be in /dashboard/+page.svelte-->
 <script lang="ts">
 	import NavBar from '$lib/components/dashboard/navbar/NavBar.svelte';
-	//import Sidebar from '$lib/components/dashboard/sidebar/Sidebar.svelte';
+	import Sidebar from '$lib/components/dashboard/sidebar/Sidebar.svelte';
 	import Canvas from '$lib/components/ui/draw/Canvas.svelte';
 
 	import { DarkMode } from '$lib/components/ui';
 </script>
 
-<Canvas />
-
-<div class="flex-row items-center">
-	<DarkMode />
-	<NavBar />
+<!-- Center this on the screen-->
+<div class="flex justify-center mt-3 z-30">
+	<div class="flex-row">
+		<DarkMode />
+		<NavBar />
+	</div>
 </div>
-<!--<Sidebar />-->
+<div>
+	<Canvas />
+	<div class="z-30">
+		<Sidebar />
+	</div>
+</div>
