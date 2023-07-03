dcb8b3756cb7d912adb7b05b3c0438817adee259
tcollect <tccoleman26@gmail.com>
2023-07-03 09:22:46 -0400

    sidebar

--- src/lib/components/dashboard/sidebar/Sidebar.svelte
+++ src/lib/components/dashboard/sidebar/Sidebar.svelte
@@ -19,14 +19,8 @@
 	}
 </script>
 
-<div
-	use:clickInside={{}}
-	on:click_inside={handleInside}
-	use:clickOutside={{}}
-	on:click_outside={handleOutside}
-	class="space-y-4"
->
-	<SidebarWrapper bind:open={$activeChart} id="sidebar">
+<div use:clickInside={{ clickInside: handleInside }} class="space-y-4">
+	<SidebarWrapper bind:open={$activeSidebar} id="sidebar">
 		<div class="space-y-3">
 			<div class="text-xs space-y-1">
 				<p>FileUpload</p>
