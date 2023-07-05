5fb5c726fff72e8e96bc4b59ff5f8b1af8ed2cbc
tcollect <tccoleman26@gmail.com>
2023-07-05 15:01:15 -0400

    page svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,29 +1,16 @@
 <script lang="ts">
-	import type { SubmitFunction } from '$app/forms';
-	import { supabaseClient } from '$lib/supabase'; //@ts-ignore
 	import Cards from '$lib/components/landing/homepage/Cards.svelte';
 	import NavBar from '$lib/components/landing/homepage/NavBar.svelte';
 	import Video from '$lib/components/landing/homepage/CallToAction.svelte';
-	import Footer from '$lib/components/landing/homepage/Footer.svelte'
+	import Footer from '$lib/components/landing/homepage/Footer.svelte';
 
 	let showDropdown = false; // State variable for dropdown menu visibility
 
 	function toggleDropdown() {
 		showDropdown = !showDropdown;
 	}
-
-	let email = 'contact@sourcechart.io';
-	let phoneNumber = 'Your phone number here';
-	let address = 'Your address here';
-
-	const submitLogout: SubmitFunction = async ({ cancel }) => {
-		const { error } = await supabaseClient.auth.signOut();
-		if (error) {
-			console.log(error);
-		}
-		cancel();
-	};
 </script>
+
 <title>SourceChart</title>
 
 <div>
