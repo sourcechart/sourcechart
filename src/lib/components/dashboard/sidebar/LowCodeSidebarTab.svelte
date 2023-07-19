8b168b345dbe6a061532fb257ba2bec98b79db0b
tcollect <tccoleman26@gmail.com>
2023-07-19 12:02:17 -0400

    Lowcodesidebar

---
+++ src/lib/components/dashboard/sidebar/LowCodeSidebarTab.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+	import {
+		DatasetDropDown,
+		AxisDropDrown,
+		Aggregator,
+		Groupby,
+		FileUpload,
+		ChartDropdown,
+		Tags
+	} from './sidebar-components';
+</script>
+
+<div class="space-y-3">
+	<div class="flex flex-col space-y-1">
+		<p class="text-xs">Upload Files</p>
+		<FileUpload />
+	</div>
+	<div class="flex flex-col space-y-1">
+		<p class="text-xs">Datasets</p>
+		<DatasetDropDown />
+	</div>
+	<div class="flex flex-col space-y-1">
+		<p class="text-xs">Choose Chart</p>
+		<ChartDropdown />
+	</div>
+	<div class="flex flex-col space-y-1">
+		<p class="text-xs">Axis</p>
+		<AxisDropDrown axis={'X'} />
+		<AxisDropDrown axis={'Y'} />
+	</div>
+	<div class="flex flex-col space-y-1">
+		<p class="text-xs">Aggregator</p>
+		<Aggregator />
+	</div>
+	<div class="flex flex-col space-y-1">
+		<p class="text-xs">Groupby</p>
+		<Groupby />
+		<Tags />
+	</div>
+</div>
--- src/lib/components/dashboard/sidebar/Sidebar.svelte
+++ src/lib/components/dashboard/sidebar/Sidebar.svelte
@@ -1,14 +1,8 @@
 <script lang="ts">
-	import {
-		DatasetDropDown,
-		AxisDropDrown,
-		Aggregator,
-		Groupby,
-		FileUpload,
-		ChartDropdown,
-		Tags
-	} from './sidebar-components';
+	import LowCodeSidebarTab from './LowCodeSidebarTab.svelte';
 	import { SidebarWrapper } from '$lib/components/ui';
+	import { Tabs, TabItem } from 'flowbite-svelte';
+
 	import { clickInside } from '$lib/actions/MouseActions';
 	import { activeSidebar } from '$lib/io/Stores';
 
@@ -19,33 +13,10 @@
 
 <div use:clickInside={{ clickInside: handleClickInside }} class="space-y-4">
 	<SidebarWrapper bind:open={$activeSidebar} id="sidebar">
-		<div class="space-y-3">
-			<div class="flex flex-col space-y-1">
-				<p class="text-xs">Upload Files</p>
-				<FileUpload />
-			</div>
-			<div class="flex flex-col space-y-1">
-				<p class="text-xs">Datasets</p>
-				<DatasetDropDown />
-			</div>
-			<div class="flex flex-col space-y-1">
-				<p class="text-xs">Choose Chart</p>
-				<ChartDropdown />
-			</div>
-			<div class="flex flex-col space-y-1">
-				<p class="text-xs">Axis</p>
-				<AxisDropDrown axis={'X'} />
-				<AxisDropDrown axis={'Y'} />
-			</div>
-			<div class="flex flex-col space-y-1">
-				<p class="text-xs">Aggregator</p>
-				<Aggregator />
-			</div>
-			<div class="flex flex-col space-y-1">
-				<p class="text-xs">Groupby</p>
-				<Groupby />
-				<Tags />
-			</div>
-		</div>
+		<Tabs style="underline" defaultClass="" contentClass="">
+			<TabItem open>
+				<LowCodeSidebarTab />
+			</TabItem>
+		</Tabs>
 	</SidebarWrapper>
 </div>
