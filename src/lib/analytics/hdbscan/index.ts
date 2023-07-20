4c2dcc787d67063db49f610cde0db8d599082f43
tcollect <tccoleman26@gmail.com>
2023-07-20 16:32:20 -0400

    Index ts

--- src/lib/analytics/hdbscan/index.ts
+++ src/lib/analytics/hdbscan/index.ts
@@ -1,4 +1,7 @@
 export { Hdbscan } from './hdbscan';
+export { UnionFind } from './unionFind';
 export { euclidean } from './metrics';
 export { mstToBinaryTree } from './mst';
-export { MetricFunction, HierarchyNode, TreeNode, MST, SingleLinkage, DebugInfo } from './types';
+export { kdTreeCoreDistance } from './coreDistance';
+export { HierarchyNode, TreeNode, DebugInfo } from './types';
+export type { MetricFunction, MST, SingleLinkage } from './types';
