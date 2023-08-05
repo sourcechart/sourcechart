9920d3dad672f77b5c32be77a5223d73023e5ff0
tcollect <tccoleman26@gmail.com>
2023-08-05 14:17:56 -0400

    WEbworker

--- src/lib/io/WebWorker.ts
+++ src/lib/io/WebWorker.ts
@@ -1,61 +1,57 @@
-//@ts-ignore
-import sqlite3InitModule from '@sqlite.org/sqlite-wasm';
+import sqlite3InitModule from 'sqlite-wasm-esm';
 
-//@ts-ignore
-const logHtml = function (cssClass, ...args: Array<any>) {
-	postMessage({
-		type: 'log',
-		payload: { cssClass, args }
-	});
+let opfsDb;
+
+self.onmessage = function (e) {
+	const { command } = e.data;
+
+	switch (command) {
+		case 'initialize':
+			initializeDb();
+			break;
+		case 'query':
+			executeQuery();
+			break;
+		default:
+			self.postMessage({ error: 'Invalid command' });
+	}
 };
 
-const log = (...args: Array<any>) => logHtml('', ...args);
-const error = (...args: Array<any>) => logHtml('error', ...args);
-
-const start = function (sqlite3: any) {
-	let db;
-	if (sqlite3.opfs) {
-		db = new sqlite3.oo1.OpfsDb('/mydb.sqlite3');
-		log('The OPFS is available.');
-	} else {
-		db = new sqlite3.oo1.DB('/mydb.sqlite3', 'ct');
-	}
-
-	try {
-		log('Create a table...');
-		db.exec('CREATE TABLE IF NOT EXISTS t(a,b)');
-		log('Insert some data using exec()...');
-		let i;
-		for (i = 20; i <= 25; ++i) {
-			db.exec({
-				sql: 'INSERT INTO t(a,b) VALUES (?,?)',
-				bind: [i, i * 2]
-			});
-		}
-		log("Query data with exec() using rowMode 'array'...");
-		db.exec({
-			sql: 'SELECT a FROM t ORDER BY a LIMIT 3',
-			rowMode: 'array', //@ts-ignore
-			callback: function (row) {
-				//@ts-ignore
-				log('row ', ++this.counter, '=', row);
-			}.bind({ counter: 0 })
+function initializeDb() {
+	sqlite3InitModule().then((sqlite3) => {
+		opfsDb = new sqlite3.opfs.OpfsDb('my-db', 'c');
+		opfsDb.open().then(() => {
+			opfsDb
+				.exec('CREATE TABLE IF NOT EXISTS test (id INTEGER PRIMARY KEY, name TEXT)')
+				.then(() => {
+					self.postMessage({ success: true, message: 'Database initialized.' });
+				})
+				.catch((error) => {
+					self.postMessage({ success: false, error: error.message });
+				});
 		});
-		console.log(db);
-	} finally {
-		db.close();
-	}
-};
+	});
+}
 
-log('Loading and initializing sqlite3 module...');
-sqlite3InitModule({
-	print: log,
-	printErr: error
-}).then(function (sqlite3: object) {
-	try {
-		start(sqlite3);
-	} catch (e: unknown) {
-		//@ts-ignore
-		error('Exception:', e.message);
+function executeQuery() {
+	if (!opfsDb) {
+		self.postMessage({ success: false, error: 'Database is not initialized.' });
+		return;
 	}
-});
+
+	opfsDb
+		.exec('INSERT INTO test (name) VALUES (?)', ['test'])
+		.then(() => {
+			opfsDb
+				.exec('SELECT * FROM test')
+				.then((result) => {
+					self.postMessage({ success: true, result });
+				})
+				.catch((error) => {
+					self.postMessage({ success: false, error: error.message });
+				});
+		})
+		.catch((error) => {
+			self.postMessage({ success: false, error: error.message });
+		});
+}
