30260afbc87c02e596b53e594f625add32433fe9
tcollect <tccoleman26@gmail.com>
2023-08-07 18:14:58 -0400

    WebWorker

--- src/lib/components/ui/draw/TestWebWorker.svelte
+++ src/lib/components/ui/draw/TestWebWorker.svelte
@@ -3,10 +3,15 @@
 
 	let syncWorker: Worker | undefined = undefined;
 
+	const onWorkerMessage = () => {
+		console.log('Cool it works out 😃');
+	};
+
 	const loadWorker = async () => {
 		const SyncWorker = await import('$lib/io/web.worker?worker');
 		syncWorker = new SyncWorker.default();
-		syncWorker.postMessage({});
+		syncWorker.postMessage({ msg: 'initialize' });
+		syncWorker.onmessage = onWorkerMessage;
 
 		console.log('syncWorker', syncWorker);
 	};
