20648f49deb5b1615598cf1296bb9a706215f89b
tcollect <tccoleman26@gmail.com>
2023-08-09 14:38:38 -0400

    NavBarWrapper

--- src/lib/components/dashboard/navbar/NavBarWrapper.svelte
+++ src/lib/components/dashboard/navbar/NavBarWrapper.svelte
@@ -1,5 +1,5 @@
 <div
-	class="rounded-md outline-red-400 fixed inset-x-0 top-0 z-50 flex h-16 items-center justify-center bg-gray-900 shadow-lg"
+	class="rounded-md fixed inset-x-0 top-3 z-50 flex h-16 items-center justify-center bg-gray-700 shadow-lg"
 >
 	<div class="flex items-center justify-between">
 		<slot />
