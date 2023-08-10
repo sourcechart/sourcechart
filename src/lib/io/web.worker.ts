1154f6f13b6323ad54a57e461ec793915e711606
tcollect <tccoleman26@gmail.com>
2023-08-10 12:37:04 -0400

    can get some results

--- src/lib/io/web.worker.ts
+++ src/lib/io/web.worker.ts
@@ -8,9 +8,7 @@ onmessage = (e) => {
 	switch (message) {
 		case 'initialize':
 			initDB();
-			break;
-		case 'query':
-			executeQuery();
+			//executeQuery();
 			break;
 		default:
 			postMessage({ error: 'Invalid command' });
@@ -24,32 +22,11 @@ const initDB = () => {
 		try {
 			opfsDb.exec('CREATE TABLE IF NOT EXISTS test (id INTEGER PRIMARY KEY, name TEXT)');
 			postMessage({ success: true, message: 'Database initialized.' });
+			opfsDb.exec('INSERT INTO test (name) VALUES (?)', ['test']);
+			let res = opfsDb.exec('SELECT * FROM test'); //@ts-ignore
+			console.log(res);
 		} catch (error) {
 			postMessage({ success: false, message: error });
 		}
 	});
-};
-
-const executeQuery = () => {
-	console.log(opfsDb);
-	if (!opfsDb) {
-		self.postMessage({ success: false, error: 'Database is not initialized.' });
-		return;
-	}
-
-	opfsDb
-		.exec('INSERT INTO test (name) VALUES (?)', ['test'])
-		.then(() => {
-			opfsDb
-				.exec('SELECT * FROM test') //@ts-ignore
-				.then((result) => {
-					self.postMessage({ success: true, result });
-				}) //@ts-ignore
-				.catch((error) => {
-					self.postMessage({ success: false, error: error.message });
-				});
-		}) //@ts-ignore
-		.catch((error) => {
-			self.postMessage({ success: false, error: error.message });
-		});
 };
