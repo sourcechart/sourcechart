ff729de51f1d373df4bb6960c9a34d44b520f538
tcollect <tccoleman26@gmail.com>
2023-08-11 12:25:21 -0400

    working the web

--- src/lib/io/web.worker.ts
+++ src/lib/io/web.worker.ts
@@ -21,21 +21,18 @@ const initDB = () => {
 	sqlite3InitModule().then((sqlite3) => {
 		let blob = getExampleBlob();
 		let filename = 'test.txt';
-
 		//@ts-ignore
 		const db = new sqlite3.opfs.OpfsDb('LocalDB', 'c');
 		db.exec(
 			`
-			CREATE TABLE IF NOT EXISTS foo (filename TEXT, binary BLOB); 
+			CREATE TABLE IF NOT EXISTS TestTable (filename TEXT, binary BLOB); 
 			`
 		);
-		db.exec(``);
+		db.exec(`
+			INSERT INTO TestTable (filename, binary) VALUES ('${filename}', '${blob}');
+		`);
 
-		/*
-		db.exec(`INSERT INTO foo (filename, binary) VALUES (${filename}, ${blob});`);
-		*/
-
-		let results = db.exec(`SELECT * FROM foo;`, { returnValue: 'resultRows' });
+		let results = db.exec(`SELECT * FROM TestTable;`, { returnValue: 'resultRows' });
 		console.log('results: ', results, 'db: ', db);
 		db.close();
 		postMessage({ message: 'finished' });
