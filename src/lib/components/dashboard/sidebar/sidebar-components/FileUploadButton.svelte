e2d183967fd9d75236c87e8c0c231c1b91fca8be
tcollect <tccoleman26@gmail.com>
2023-08-14 19:10:26 -0400

    File UPload button

--- src/lib/components/dashboard/sidebar/sidebar-components/FileUploadButton.svelte
+++ src/lib/components/dashboard/sidebar/sidebar-components/FileUploadButton.svelte
@@ -4,6 +4,7 @@
 	import { fileUploadStore } from '$lib/io/Stores';
 	import { bufferToHex } from '$lib/io/HexOps';
 	import { onMount } from 'svelte';
+	import { Button } from 'flowbite-svelte';
 
 	let syncWorker: Worker | undefined = undefined;
 	let files: ListGroupItemType | null = null;
@@ -26,7 +27,7 @@
 			size: fileSize,
 			fileextension: filename.split('.').pop()
 		};
-		$fileUploadStore = [...$fileUploadStore, tableColumnsSize];
+		fileUploadStore.update((files) => [...files, tableColumnsSize]);
 	};
 
 	const uploadFiles = async (e: Event) => {
@@ -48,8 +49,34 @@
 			});
 		}
 	};
+
 	onMount(loadWorker);
 </script>
+
+<Button color="dark">
+	<div class="flex justify-between space-x-2">
+		<svg
+			class="text-gray-800 dark:text-white"
+			x="0px"
+			y="0px"
+			width="18"
+			height="18"
+			aria-hidden="true"
+			xmlns="http://www.w3.org/2000/svg"
+			fill="none"
+			viewBox="0 0 18 18"
+		>
+			<path
+				stroke="currentColor"
+				stroke-linecap="round"
+				stroke-linejoin="round"
+				stroke-width="2"
+				d="M9 1v16M1 9h16"
+			/>
+		</svg>
+		<p>Add Data</p>
+	</div>
+</Button>
 
 <Fileupload id="multiple_files" multiple on:change={uploadFiles} />
 <Listgroup {files} class="mt-2" />
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,7 @@ const config = {
 	mode: 'jit',
 	content: [
 		'./src/**/*.{html,js,svelte,ts}',
-		'./node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}'
+		'./node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}',
 	],
 
 	plugins: [require('flowbite/plugin')],
