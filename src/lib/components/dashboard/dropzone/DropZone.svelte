d3b2daafe37c57e6aa826ce609f3ef8c8f158685
tcollect <tccoleman26@gmail.com>
2023-08-14 20:53:14 -0400

    Uploading dashboard

--- src/lib/components/dashboard/dropzone/DropZone.svelte
+++ src/lib/components/dashboard/dropzone/DropZone.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import { Dropzone } from 'flowbite-svelte';
 	import { generateID } from '$lib/io/GenerateID';
-	import { fileUploadStore } from '$lib/io/Stores';
+	import { fileUploadStore, activeDropZone } from '$lib/io/Stores';
 	import { bufferToHex } from '$lib/io/HexOps';
 	import { onMount } from 'svelte';
 
@@ -87,34 +87,36 @@
 	onMount(loadWorker);
 </script>
 
-<Dropzone
-	id="dropzone"
-	on:drop={dropHandle}
-	on:dragover={(event) => {
-		event.preventDefault();
-	}}
-	on:change={handleChange}
->
-	<svg
-		aria-hidden="true"
-		class="mb-3 w-10 h-10 text-gray-400"
-		fill="none"
-		stroke="currentColor"
-		viewBox="0 0 24 24"
-		xmlns="http://www.w3.org/2000/svg"
-		><path
-			stroke-linecap="round"
-			stroke-linejoin="round"
-			stroke-width="2"
-			d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"
-		/></svg
+{#if $activeDropZone}
+	<Dropzone
+		id="dropzone"
+		on:drop={dropHandle}
+		on:dragover={(event) => {
+			event.preventDefault();
+		}}
+		on:change={handleChange}
 	>
-	{#if value.length === 0}
-		<p class="mb-2 text-sm text-gray-500 dark:text-gray-400">
-			<span class="font-semibold">Click to upload</span> or drag and drop
-		</p>
-		<p class="text-xs text-gray-500 dark:text-gray-400">CSV, Parquet, Text, JSON</p>
-	{:else}
-		<p>{showFiles(value)}</p>
-	{/if}
-</Dropzone>
+		<svg
+			aria-hidden="true"
+			class="mb-3 w-10 h-10 text-gray-400"
+			fill="none"
+			stroke="currentColor"
+			viewBox="0 0 24 24"
+			xmlns="http://www.w3.org/2000/svg"
+			><path
+				stroke-linecap="round"
+				stroke-linejoin="round"
+				stroke-width="2"
+				d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"
+			/></svg
+		>
+		{#if value.length === 0}
+			<p class="mb-2 text-sm text-gray-500 dark:text-gray-400">
+				<span class="font-semibold">Click to upload</span> or drag and drop
+			</p>
+			<p class="text-xs text-gray-500 dark:text-gray-400">CSV, Parquet, Text, JSON</p>
+		{:else}
+			<p>{showFiles(value)}</p>
+		{/if}
+	</Dropzone>
+{/if}
