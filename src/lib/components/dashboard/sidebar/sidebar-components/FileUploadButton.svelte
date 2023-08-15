064160a20a27ceda3b57ce6203cffdf6f3859116
tcollect <tccoleman26@gmail.com>
2023-08-14 20:04:32 -0400

    DropZone

--- src/lib/components/dashboard/sidebar/sidebar-components/FileUploadButton.svelte
+++ src/lib/components/dashboard/sidebar/sidebar-components/FileUploadButton.svelte
@@ -4,52 +4,6 @@
 	import { bufferToHex } from '$lib/io/HexOps';
 	import { onMount } from 'svelte';
 	import { Button } from 'flowbite-svelte';
-
-	let syncWorker: Worker | undefined = undefined;
-	let files: ListGroupItemType | null = null;
-	interface ListGroupItemType {
-		current?: boolean;
-		disabled?: boolean;
-		href?: string;
-		[propName: string]: any;
-	}
-
-	const loadWorker = async () => {
-		const SyncWorker = await import('$lib/io/web.worker?worker');
-		syncWorker = new SyncWorker.default();
-	};
-
-	const createFileStore = (filename: string, fileSize: number, dataID: string) => {
-		var tableColumnsSize = {
-			filename: filename,
-			datasetID: dataID,
-			size: fileSize,
-			fileextension: filename.split('.').pop()
-		};
-		fileUploadStore.update((files) => [...files, tableColumnsSize]);
-	};
-
-	const uploadFiles = async (e: Event) => {
-		var target = e.target as HTMLInputElement;
-		var f = (target.files as FileList)[0];
-		var arrayBuffer = await f.arrayBuffer();
-		var id = generateID();
-		var hex = bufferToHex(arrayBuffer);
-
-		createFileStore(f.name, f.size, id);
-		if (syncWorker) {
-			syncWorker.postMessage({
-				filename: f.name,
-				size: f.size,
-				id: id,
-				message: 'initialize',
-				hexadecimal: hex,
-				fileextension: f.name.split('.').pop()
-			});
-		}
-	};
-
-	onMount(loadWorker);
 </script>
 
 <Button id="multiple_files" color="dark">
