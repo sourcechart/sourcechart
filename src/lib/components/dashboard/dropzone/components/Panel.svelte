42a1c7542c5485efe0c3a49dc6ea136888258d0b
tcollect <tccoleman26@gmail.com>
2023-08-14 21:58:33 -0400

    Uploading panel

--- src/lib/components/dashboard/dropzone/components/Panel.svelte
+++ src/lib/components/dashboard/dropzone/components/Panel.svelte
@@ -1,14 +1,22 @@
+<script>
+	import { Tabs, TabItem } from 'flowbite-svelte';
+</script>
+
 <div
-	class="relative bg-white px-6 pb-8 pt-10 shadow-xl ring-1 ring-gray-900/5 sm:mx-auto sm:max-w-lg sm:rounded-lg sm:px-10"
+	class="relative bg-white dark:bg-slate-700 px-6 pb-8 pt-10 shadow-xl ring-1 ring-gray-900/5 sm:mx-auto sm:max-w-lg sm:rounded-lg sm:px-10"
 >
 	<div class="mx-auto max-w-md">
 		<h1>Add Data to Plot</h1>
-		<div class="divide-y divide-gray-300/50">
-			<div class="space-y-6 py-8 text-base leading-7 text-gray-600" />
-			<div class="pt-8 text-base font-semibold leading-7">
-				<slot />
-				<p class="text-gray-900">Want to dig deeper into Tailwind? Hello Hellow Hellow Hellow</p>
-			</div>
-		</div>
+		<Tabs>
+			<TabItem title="Load Files" open>
+				<div class="pt-8 text-base font-semibold leading-7">
+					<slot />
+					<p class="text-gray-900">Want to dig deeper into Tailwind? Hello Hellow Hellow Hellow</p>
+				</div>
+			</TabItem>
+			<TabItem title="External Datasets">
+				<p>Tab 2 content</p>
+			</TabItem>
+		</Tabs>
 	</div>
 </div>
