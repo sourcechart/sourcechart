d9c664ec805e1f55b58ce9ef720e8ecbd880432c
tcollect <tccoleman26@gmail.com>
2023-08-14 21:13:47 -0400

    DropDownWrapper

---
+++ src/lib/components/dashboard/dropzone/DropZoneWrapper.svelte
@@ -0,0 +1,19 @@
+<div
+	class="relative flex min-h-screen flex-col justify-center overflow-hidden bg-gray-50 py-6 sm:py-12"
+>
+	<div
+		class="relative bg-white px-6 pb-8 pt-10 shadow-xl ring-1 ring-gray-900/5 sm:mx-auto sm:max-w-lg sm:rounded-lg sm:px-10"
+	>
+		<div class="mx-auto max-w-md">
+			<h1>Add Data to Plot</h1>
+			<div class="divide-y divide-gray-300/50">
+				<div class="space-y-6 py-8 text-base leading-7 text-gray-600">
+					<slot />
+				</div>
+				<div class="pt-8 text-base font-semibold leading-7">
+					<p class="text-gray-900">Want to dig deeper into Tailwind?</p>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
