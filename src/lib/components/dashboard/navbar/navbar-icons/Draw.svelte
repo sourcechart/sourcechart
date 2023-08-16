825d40958783365f8ada541bf7544d819cfa0fce
tcollect <tccoleman26@gmail.com>
2023-08-16 14:38:40 -0400

    Updating Draw and Eraser SVG

--- src/lib/components/dashboard/navbar/navbar-icons/Draw.svelte
+++ src/lib/components/dashboard/navbar/navbar-icons/Draw.svelte
@@ -14,7 +14,23 @@
 	on:click={clickButton}
 	class={mode === 'drawRectangle' ? 'dark:bg-gray-700' : 'dark:bg-gray-800'}
 >
-	<svg x="0px" y="0px" width="30" height="30" viewBox="0 0 30 30">
-		<rect x="5" y="5" width="20" height="20" fill="rgba(0,0,0,0)" stroke="black" />
-	</svg>
+	<svg width="24px" height="24px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"
+		><g id="SVGRepo_bgCarrier" stroke-width="0" /><g
+			id="SVGRepo_tracerCarrier"
+			stroke-linecap="round"
+			stroke-linejoin="round"
+		/><g id="SVGRepo_iconCarrier">
+			<rect
+				x="4"
+				y="4"
+				width="16"
+				height="16"
+				rx="2"
+				stroke="#ffffff"
+				stroke-width="1"
+				stroke-linecap="round"
+				stroke-linejoin="round"
+			/>
+		</g></svg
+	>
 </button>
