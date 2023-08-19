6bd0c2a8101d750639ffb5dd8cdbba3051020e0a
tcollect <tccoleman26@gmail.com>
2023-08-18 22:17:33 -0400

    CORS

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,11 @@ import { defineConfig } from 'vitest/config';
 
 /** @type {import('vite').Plugin} */
 const viteServerConfig = {
-	name: 'log-request-middleware', //@ts-ignore
-	configureServer(server) {
-		//@ts-ignore
+	name: 'add headers',
+	configureServer: (server) => {
 		server.middlewares.use((req, res, next) => {
-			res.setHeader('Access-Control-Allow-Origin', '*');
-			res.setHeader('Access-Control-Allow-Methods', 'GET');
+			//			res.setHeader('Access-Control-Allow-Origin', '*');
+			//			res.setHeader('Access-Control-Allow-Methods', 'GET');
 			res.setHeader('Cross-Origin-Opener-Policy', 'same-origin');
 			res.setHeader('Cross-Origin-Embedder-Policy', 'require-corp');
 			next();
@@ -19,7 +18,7 @@ const viteServerConfig = {
 
 /** @type {import('@sveltejs/kit').Config} */
 export default defineConfig({
-	plugins: [viteServerConfig, sveltekit()],
+	plugins: [sveltekit(), viteServerConfig],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	},
