0ba094804d4f4eacd0c70daee45113d7ea0203e1
tcollect <tccoleman26@gmail.com>
2023-08-19 10:35:58 -0400

    viteConfig

--- vite.config.ts
+++ vite.config.ts
@@ -7,8 +7,8 @@ const viteServerConfig = {
 	name: 'add headers',
 	configureServer: (server) => {
 		server.middlewares.use((req, res, next) => {
-			//			res.setHeader('Access-Control-Allow-Origin', '*');
-			//			res.setHeader('Access-Control-Allow-Methods', 'GET');
+			res.setHeader('Access-Control-Allow-Origin', '*');
+			res.setHeader('Access-Control-Allow-Methods', 'GET');
 			res.setHeader('Cross-Origin-Opener-Policy', 'same-origin');
 			res.setHeader('Cross-Origin-Embedder-Policy', 'require-corp');
 			next();
