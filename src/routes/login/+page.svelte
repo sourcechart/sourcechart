3ff3e1d6897ea188c36e4bfb501c41cda10b7aab
tcollect <tccoleman26@gmail.com>
2023-08-19 21:01:41 -0400

    addin

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -6,7 +6,7 @@
 
 <div class="relative flex min-h-screen flex-col justify-center overflow-hidden py-6 sm:py-12">
 	<div
-		class="relative bg-white px-6 border pb-8 pt-10 shadow-xl round ring-1 ring-gray-900 outline-gray-900/5 sm:mx-auto sm:max-w-lg sm:rounded-lg sm:px-10"
+		class="relative px-6 border pb-8 pt-10 shadow-xl round ring-1 ring-gray-900 outline-gray-900/5 sm:mx-auto sm:max-w-lg sm:rounded-lg sm:px-10"
 	>
 		<Auth
 			supabaseClient={data.supabase}
