0991d7beb7e8d339ac8908ae1d38292645d0aa84
tcollect <tccoleman26@gmail.com>
2023-08-19 21:05:17 -0400

    redirect

--- src/routes/login/+page.server.ts
+++ src/routes/login/+page.server.ts
@@ -1,25 +1,14 @@
-// src/routes/login/+page.server.ts
-export const actions = {
-	default: async ({ request, url, locals: { supabase } }) => {
-		const formData = await request.formData();
-		const email = formData.get('email') as string;
-		const password = formData.get('password') as string;
+// src/routes/+page.server.ts
+import { redirect } from '@sveltejs/kit';
+import type { PageServerLoad } from './$types';
 
-		const { error } = await supabase.auth.signUp({
-			email,
-			password,
-			options: {
-				emailRedirectTo: `${url.origin}/auth/callback`
-			}
-		});
+export const load: PageServerLoad = async ({ url, locals: { getSession } }) => {
+	const session = await getSession();
 
-		if (error) {
-			return fail(500, { message: 'Server error. Try again later.', success: false, email });
-		}
-
-		return {
-			message: 'Please check your email for a magic link to log into the website.',
-			success: true
-		};
+	// if the user is already logged in return them to the account page
+	if (session) {
+		throw redirect(303, '/dashboard');
 	}
+
+	return { url: url.origin };
 };
