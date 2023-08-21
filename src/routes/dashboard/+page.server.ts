81250bee36edea8866e57b44ebae920f0e971abd
tcollect <tccoleman26@gmail.com>
2023-08-21 13:41:03 -0400

    foo

--- src/routes/dashboard/+page.server.ts
+++ src/routes/dashboard/+page.server.ts
@@ -8,14 +8,16 @@ export const load: PageServerLoad = async ({ locals: { supabase, getSession } })
 		throw redirect(303, '/login');
 	}
 
-	const { error } = await supabase
+	/*const { error } = await supabase
 		.from('countries')
 		.insert({ id: 250, name: 'test', iso2: '', iso3: '', continent: 'Africa', local_name: '' });
+	*/
 
+	const { error } = await supabase.from('test').insert({ name: 'test' });
 	console.log(error);
-	const { data } = await supabase.from('countries').select('name');
 
+	const { data } = await supabase.from('test').select('name');
 	return {
-		countries: data ?? []
+		name: data ?? []
 	};
 };
