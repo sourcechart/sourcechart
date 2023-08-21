a4ecfb92bc79c04e0afe31af5ca0b867b2f35c8d
tcollect <tccoleman26@gmail.com>
2023-08-21 11:39:56 -0400

    Server.ts

--- src/routes/dashboard/+page.server.ts
+++ src/routes/dashboard/+page.server.ts
@@ -1,12 +1,17 @@
 import { redirect } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 
-export const load: PageServerLoad = async ({ url, locals: { getSession } }) => {
+export const load: PageServerLoad = async ({ locals: { supabase, getSession } }) => {
 	const session = await getSession();
 
 	if (!session) {
 		throw redirect(303, '/login');
 	}
 
-	return { url: url.origin };
+	const { data, error } = await supabase
+		.from('sessions')
+		.insert({ userId: session.user.id, id: session.user.session_id })
+		.select();
+
+	console.log(data);
 };
--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -3,22 +3,6 @@
 	import { setLocalStorage } from '$lib/io/Storage';
 
 	setLocalStorage('color-theme', 'dark');
-
-	export let data;
-
-	const addDataToDatabase = async () => {
-		const { error } = await data.supabase.from('users').insert({
-			id: data.session?.user.id,
-			email: data.session?.user.email, //@ts-ignore
-			session_id: data.session?.user.session_id
-		});
-
-		console.log(error);
-	};
-
-	$: if (data.session) {
-		addDataToDatabase();
-	}
 </script>
 
 <Dashboard />
