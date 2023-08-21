9d56ae1df197c8eb3eda52a4b1cab7ec9358edac
tcollect <tccoleman26@gmail.com>
2023-08-20 20:02:14 -0400

    let's see if this works

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -1,9 +1,22 @@
 <script lang="ts">
-	import { setLocalStorage } from '$lib/io/Storage';
 	import Dashboard from '$lib/components/dashboard/dashboard/Dashboard.svelte';
+	import { setLocalStorage } from '$lib/io/Storage';
 
 	setLocalStorage('color-theme', 'dark');
+
 	export let data;
+
+	const addDataToDatabase = async () => {
+		await data.supabase.from('users').insert({
+			id: data.session?.user.id,
+			email: data.session?.user.email,
+			session_id: data.session?.user.session_id
+		});
+	};
+
+	$: if (data.session) {
+		addDataToDatabase();
+	}
 </script>
 
 <Dashboard />
