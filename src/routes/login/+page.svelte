eb54a65e8fda6b80f79f492282fd5b8993760925
tcollect <tccoleman26@gmail.com>
2023-08-30 19:07:02 -0400

    +page svelte

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,70 +1,24 @@
 <script lang="ts">
+	import { Auth } from '@supabase/auth-ui-svelte';
+	import { ThemeSupa } from '@supabase/auth-ui-shared';
 	export let data;
-	let { supabase } = data;
-	$: ({ supabase } = data);
-
-	let email: string;
-	let password: string;
-
-	type Provider = 'google';
-
-	const handleSignIn = async () => {
-		await supabase.auth.signInWithPassword({
-			email,
-			password
-		});
-	};
-
-	const handleSignUp = async () => {
-		await supabase.auth.signUp({
-			email,
-			password,
-			options: {
-				emailRedirectTo: `${location.origin}/auth/callback`
-			}
-		});
-	};
-
-	const handleOAuthLogin = async (provider: Provider) => {
-		const { data, error } = await supabase.auth.signInWithOAuth({ provider: provider });
-	};
 </script>
 
-<!-- Use Tailwind CSS classes for styling -->
-<div class="flex justify-center items-center h-screen bg-gray-200">
-	<div class="bg-white p-8 rounded-lg shadow-md w-96">
-		<h1 class="text-2xl mb-4">Login</h1>
+<div class="relative flex min-h-screen flex-col justify-center overflow-hidden py-6 sm:py-12">
+	<div
+		class="relative px-6 border pb-8 pt-10 shadow-xl round ring-1 ring-gray-900 outline-gray-900/5 sm:mx-auto sm:max-w-lg sm:rounded-lg sm:px-10"
+	>
 		<img src="logo1.png" alt="Logo" class="mx-auto mb-4 w-48" />
-
-		<form>
-			<div class="mb-4">
-				<label for="username" class="block text-sm font-medium text-gray-600">Username</label>
-				<input
-					id="username"
-					type="text"
-					class="mt-1 p-2 w-full rounded-md border"
-					bind:value={email}
-				/>
-			</div>
-
-			<div class="mb-4">
-				<label for="password" class="block text-sm font-medium text-gray-600">Password</label>
-				<input
-					id="password"
-					type="password"
-					class="mt-1 p-2 w-full rounded-md border"
-					bind:value={password}
-				/>
-			</div>
-
-			<div>
-				<button
-					type="submit"
-					class="w-full bg-blue-500 text-white p-2 rounded-md hover:bg-blue-600 focus:outline-none focus:border-blue-700 focus:ring focus:ring-blue-200"
-				>
-					Login
-				</button>
-			</div>
-		</form>
+		<Auth
+			supabaseClient={data.supabase}
+			view="magic_link"
+			magicLink={true}
+			showLinks={false}
+			redirectTo={`${data.url}/auth/callback`}
+			providers={['google']}
+			appearance={{
+				theme: ThemeSupa
+			}}
+		/>
 	</div>
 </div>
