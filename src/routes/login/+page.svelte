559944bc2a51cb438645eaec3e6e42cfa4bb14ee
tcollect <tccoleman26@gmail.com>
2023-08-30 21:40:27 -0400

    this works, don't like the color though

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -7,7 +7,7 @@
 <div class="flex justify-center items-center h-screen bg-gray-200">
 	<div class="bg-white p-8 rounded-lg shadow-md w-96">
 		<div class="flex flex-col items-center">
-			<a href="#">
+			<a href={data.url.concat('/', 'login')}>
 				<img src="logo1.png" alt="Logo" class="mx-auto mb-4 w-52" />
 			</a>
 			<h1 class="text-3xl mb-4 text-center">Get Started</h1>
