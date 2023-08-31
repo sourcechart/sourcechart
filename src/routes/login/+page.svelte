eb7391a661aa05ef14e3382a68ff21636dccfb7e
tcollect <tccoleman26@gmail.com>
2023-08-30 21:26:32 -0400

    login page

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -4,11 +4,12 @@
 	export let data;
 </script>
 
-<div class="relative flex min-h-screen flex-col justify-center overflow-hidden py-6 sm:py-12">
-	<div
-		class="relative px-6 border pb-8 pt-10 shadow-xl round ring-1 ring-gray-900 outline-gray-900/5 sm:mx-auto sm:max-w-lg sm:rounded-lg sm:px-10"
-	>
-		<img src="logo1.png" alt="Logo" class="mx-auto mb-4 w-48" />
+<div class="flex justify-center items-center h-screen bg-gray-200">
+	<div class="bg-white p-8 rounded-lg shadow-md w-96">
+		<div class="flex flex-col items-center">
+			<img src="logo1.png" alt="Logo" class="mx-auto mb-4 w-52" />
+			<h1 class="text-3xl mb-4 text-center">Get Started</h1>
+		</div>
 		<Auth
 			supabaseClient={data.supabase}
 			view="magic_link"
