4dc164c6c7fc7e5f4ab7e20b17d1eb773f05a516
tcollect <tccoleman26@gmail.com>
2023-09-09 11:27:18 -0400

    updating fitler dropdown

--- src/lib/components/dashboard/sidebar/sidebar-components/filter-components/FilterDropdown.svelte
+++ src/lib/components/dashboard/sidebar/sidebar-components/filter-components/FilterDropdown.svelte
@@ -2,20 +2,19 @@
 	//@ts-ignore
 	import Dropdown from 'flowbite-svelte/Dropdown.svelte'; //@ts-ignore
 	import Button from 'flowbite-svelte/Button.svelte'; //@ts-ignore
-
 	import DropdownItem from 'flowbite-svelte/DropdownItem.svelte';
-
 	import { allCharts, clickedChartIndex } from '$lib/io/Stores';
-	export let items: any[] = [];
 
+	export let items: any[] = [];
 	export let column: string;
+
 	$: i = clickedChartIndex();
 
 	const addFilterToChart = (item: string) => {
-		const existingFilter = $allCharts[$i].filterColumns.find((filter) => filter.column === item);
+		const existingFilter = $allCharts[$i].filterColumns.find((filter) => filter.column === column);
 
 		if (existingFilter) {
-			$allCharts[$i].filterColumns.find((filter) => filter.column === item).value = {
+			$allCharts[$i].filterColumns.find((filter) => filter.column === column).value = {
 				item: item
 			};
 		} else {
