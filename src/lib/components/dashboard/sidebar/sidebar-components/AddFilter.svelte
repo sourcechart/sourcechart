7b25fc0783390ab8e011c5ff3308aa31796a7fc6
tcollect <tccoleman26@gmail.com>
2023-09-09 21:08:30 -0400

    Adding fitler

--- src/lib/components/dashboard/sidebar/sidebar-components/AddFilter.svelte
+++ src/lib/components/dashboard/sidebar/sidebar-components/AddFilter.svelte
@@ -4,19 +4,20 @@
 	import Filter from './filter-components/Filter.svelte';
 	import { PlusSolid } from 'flowbite-svelte-icons';
 
-	let components: any[] = [];
+	export let addFilterDistance: number = 0;
+	let components: { component: any; distance: number }[] = [];
 
 	const addFilterToSidebar = () => {
-		components = [...components, Filter];
+		components = [...components, { component: Filter, distance: addFilterDistance }];
 	};
 </script>
 
-{#each components as Component}
+{#each components as { component: Component, distance }}
 	<div class="py-2">
-		<svelte:component this={Component} />
+		<svelte:component this={Component} addFilterDistance={distance} />
 	</div>
 {/each}
-<Button on:click={addFilterToSidebar}
-	>Add Filter
+<Button on:click={addFilterToSidebar}>
+	Add Filter
 	<PlusSolid class="w-3 h-3 ml-2 text-white dark:text-white" />
 </Button>
