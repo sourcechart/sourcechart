1bffcbe15357f746b1e8849b59c431e0842f95a2
tcollect <tccoleman26@gmail.com>
2023-09-10 11:58:22 -0400

    foitler component

--- src/lib/components/dashboard/sidebar/sidebar-components/filter-components/Filter.svelte
+++ src/lib/components/dashboard/sidebar/sidebar-components/filter-components/Filter.svelte
@@ -138,20 +138,28 @@
 
 <div class="w-full p-4 bg-gray-500 rounded-sm">
 	<div class="flex justify-between items-center">
-		<div class="dropdown" on:click={toggleDropdown} on:keypress={null}>
-			<button>
+		<div class="relative group" on:click={toggleDropdown} on:keypress={null}>
+			<button class="bg-gray-200 px-3 py-2 rounded text-black hover:bg-gray-300">
 				{selectedColumn}
 			</button>
-
-			<div class={`dropdown-menu ${placement} ${isDropdownOpen ? 'open' : ''}`}>
+			<div
+				class={`absolute w-full mt-2 bg-white border
+						border-gray-200 rounded shadow-lg transform transition-transform origin-top 
+                        ${
+													isDropdownOpen ? 'translate-y-0 opacity-100' : 'translate-y-1/2 opacity-0'
+												}`}
+			>
 				{#each $columns as column (column)}
-					<button on:click={() => addColumnToFilter(column)}>
+					<button
+						class="block w-full text-left px-3 py-2 text-black hover:bg-gray-200"
+						on:click={() => addColumnToFilter(column)}
+					>
 						{column}
 					</button>
 				{/each}
 			</div>
-			<CloseButton on:click={handleClose} />
 		</div>
+		<CloseButton on:click={handleClose} />
 	</div>
 	<div class="mt-4">
 		{#if showRange}
