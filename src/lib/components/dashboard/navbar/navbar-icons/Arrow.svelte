4fac58b78c07f80aaef8c69ee159b7493bfad4fa
tcollect <tccoleman26@gmail.com>
2023-09-16 11:47:52 -0400

    Arrow

--- src/lib/components/dashboard/navbar/navbar-icons/Arrow.svelte
+++ src/lib/components/dashboard/navbar/navbar-icons/Arrow.svelte
@@ -19,7 +19,7 @@
 	<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" {...$$props}
 		><path
 			fill="none"
-			stroke="currentColor"
+			stroke="#9CA3AF"
 			stroke-linecap="round"
 			stroke-linejoin="round"
 			stroke-width="1.5"
