3d0748b17f493b2bf37301f564b868ad5c90197c
tcollect <tccoleman26@gmail.com>
2023-09-22 12:51:14 -0400

    +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,4 @@
-<div class="w-full h-full bg-gradient-to-br from-neutral-900 via-neutral-800 to-[#2e2e2e]">
+<div class="w-full h-full bg-gradient-to-b from-neutral-900 via-neutral-800 to-[#3e3e3e]">
 	<section id="navbar" class="py-2 h-22 px-2 sm:px-4 md:px-8 lg:px-10">
 		<div class="navbar w-full flex justify-between items-center">
 			<div
@@ -18,38 +18,42 @@
 			</div>
 		</div>
 	</section>
-	<section id="space" class="lg:h-44 sm:h-40 w-full" />
+	<section id="space" class="lg:h-44 md:h-24 sm:h-20 w-full" />
 	<section class="py-4 px-2 sm:px-4 md:px-8 lg:px-10">
 		<div
 			class="text-white font-maven flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-6 ml-2 sm:ml-3 md:ml-4 lg:ml-8 mr-2 sm:mr-3 md:mr-4 lg:mr-4"
 		>
 			<div class="mb-4 md:mb-0 space-y-4">
-				<span
-					class="shadow-lg font-medium text-3xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-6xl"
-				>
-					Slice through the noise.
-				</span>
-				<div class="mt-2 md:mt-4">
-					<span class="shadow-md font-thin text-lg sm:text-xl md:text-xl lg:text-2xl">
-						SourceChart processes gigabytes of data locally that moves as fast as you think.
+				<div class="shadow">
+					<span class="font-medium block text-3xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl">
+						Slice through the noise.
 					</span>
-				</div>
-				<div class="flex flex-wrap items-center mt-4 space-x-4">
-					<button
-						class="bg-blue-700 border border-white px-4 py-2 text-sm sm:text-base md:text-base lg:text-lg shadow-lg leading-normal tracking-tight hover:bg-blue-800"
-					>
-						<span> Sign Up </span>
-					</button>
-					<button
-						class="text-white border border-white px-4 py-2 text-sm sm:text-base md:text-base lg:text-lg shadow-lg leading-normal tracking-tight hover:bg-gray-700"
-					>
-						<span>Learn More</span>
-					</button>
+					<div class="mt-2 md:mt-4">
+						<span class="font-thin text-lg">
+							SourceChart processes gigabytes of data locally that moves as fast as you think.
+						</span>
+					</div>
+
+					<div class="flex flex-wrap items-center mt-4 space-x-4">
+						<button
+							class="bg-blue-700 border border-white px-4 py-2 text-sm sm:text-base md:text-base lg:text-lg shadow-lg leading-normal tracking-tight hover:bg-blue-800"
+						>
+							<span> Sign Up </span>
+						</button>
+						<button
+							class="text-white border border-white px-4 py-2 text-sm sm:text-base md:text-base lg:text-lg shadow-lg leading-normal tracking-tight hover:bg-gray-700"
+						>
+							<span>Learn More</span>
+						</button>
+					</div>
 				</div>
 			</div>
-			<!-- Hero Image does not work well-->
-			<div class="mt-4 md:mt-0 flex-shrink-0">
-				<img class="-mt-4 top-0 left-0" src="hero2.png" alt="Logo" />
+			<div
+				class="mt-4 md:mt-0 flex-grow md:w-1/2 lg:w-2/5 xl:w-1/2 2xl:w-3/5 relative md:h-60 lg:h-1/3 xl:h-1/2 2xl:h-2/3"
+			>
+				<div class="w-full h-full">
+					<img class="w-full h-full object-cover object-center" src="hero2.png" alt="Logo" />
+				</div>
 			</div>
 		</div>
 	</section>
