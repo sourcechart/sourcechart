83181eee07a17382ef24f62969c7e136969afb31
tcollect <tccoleman26@gmail.com>
2023-09-23 11:39:30 -0400

    updating dashboard

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,7 @@
 	import Arrow from '$lib/components/landing/homepage/icons/Arrow.svelte';
 	import Hand from '$lib/components/landing/homepage/icons/Hand.svelte';
 	import InfinityIcon from '$lib/components/landing/homepage/icons/InfinityIcon.svelte';
+	import Security from '$lib/components/landing/homepage/icons/Security.svelte';
 	import { goto } from '$app/navigation';
 
 	let features = [
@@ -16,15 +17,20 @@
 			body: 'Everything is right where you would  expect it to be.'
 		},
 		{
-			component: InfinityIcon,
+			component: Security,
 			header: 'Your data is yours',
-			body: 'Data never leaves your computer. This ensures  that  your data stays private'
+			body: 'Data never leaves your computer. This ensures that your data stays private'
+		},
+		{
+			component: InfinityIcon,
+			header: 'Process gigabytes of data',
+			body: 'Our platform is designed to handle large amounts of data. We are able to process gigabytes of data in seconds.'
 		}
 	];
 </script>
 
 <div
-	class="w-full h-full font-maven bg-gradient-to-br from-neutral-900 via-neutral-800 to-[#3e3e3e]"
+	class="w-full h-full font-maven bg-gradient-to-br bg-neutral-800 from-neutral-900 via-neutral-800 to-[#3e3e3e]"
 >
 	<!-- NavBar -->
 	<section id="navbar" class="py-4 px-6 flex justify-between items-center">
@@ -32,7 +38,7 @@
 			<img src="logo1.png" alt="Logo" class="w-full h-auto" />
 		</a>
 		<button
-			class="text-neutral-300 bg-transparent border border-gray-400 hover:border-neutral-50 px-12 py-3 text-sm font-medium hover:bg-neutral-700"
+			class="text-neutral-300 bg-transparent border border-gray-400 hover:border-neutral-50 px-8 sm:px-10 md:px-12 py-2 sm:py-3 text-sm font-medium hover:bg-neutral-700"
 			on:click={() => goto('/dashboard')}
 		>
 			Login
@@ -45,6 +51,7 @@
 		style="background-image: url('backlighting.png'); background-size: cover; background-repeat: no-repeat;"
 	>
 		<div class="sm:mt-16" id="space" />
+
 		<!-- Hero Section -->
 		<section class=" h-4/5 py-4 px-2 sm:px-4 md:px-8 lg:px-10 bg-center bg-cover">
 			<div
@@ -65,17 +72,18 @@
 						</div>
 						<div class="flex flex-wrap items-center mt-4 space-x-4">
 							<button
-								class="bg-blue-700 border border-white hover:border-neutral-50 hover:text-neutral-50 px-4 py-2 text-xs sm:text-sm md:text-base lg:text-lg shadow-lg leading-normal tracking-tight hover:bg-blue-600"
+								class="bg-purple-700 border border-white hover:border-neutral-50 hover:text-neutral-50 px-8 sm:px-10 md:px-12 py-2 sm:py-3 text-xs sm:text-sm md:text-base lg:text-lg shadow-lg leading-normal tracking-tight hover:bg-purple-600"
 							>
 								<span> Try Now </span>
 							</button>
-							<button
-								class="text-neutral-300 border border-white px-4 py-2 text-xs sm:text-sm md:text-base lg:text-lg shadow-lg leading-normal tracking-tight hover:bg-neutral-800 hover:text-neutral-50 hover:border-neutral-50"
-							>
-								<a href="#features-section">
+
+							<a href="#features-section">
+								<button
+									class="text-neutral-300 border border-white px-8 sm:px-10 md:px-12 py-2 sm:py-3 text-xs sm:text-sm md:text-base lg:text-lg shadow-lg leading-normal tracking-tight hover:bg-neutral-800 hover:text-neutral-50 hover:border-neutral-50"
+								>
 									<span>Learn More</span>
-								</a>
-							</button>
+								</button>
+							</a>
 						</div>
 					</div>
 				</div>
@@ -88,8 +96,7 @@
 				</div>
 			</div>
 		</section>
-		<!--SPACE-->
-		<div class="h-24" id="space" />
+		<div class="h-32" id="space" />
 
 		<!-- Features Section -->
 		<section id="features-section">
@@ -98,7 +105,7 @@
 				Most dashboards are designed for data visualization and monitoring purposes. SourceChart is
 				designed to find insights.
 			</p>
-			<div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+			<div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
 				{#each features as feature}
 					<div class="bg-neutral-800 p-6 rounded-lg space-y-4 text-white mb-4">
 						<svelte:component this={feature.component} />
@@ -108,14 +115,25 @@
 				{/each}
 			</div>
 		</section>
-		<!--SPACE-->
 		<div class="h-24" id="space" />
+
+		<!-- Video Section -->
+		<section>
+			<div class="flex justify-center items-center rounded-lg overflow-hidden">
+				<video autoplay loop muted class="max-w-full h-auto">
+					<source src="demoMP4.mp4" type="video/mp4" />
+					Your browser does not support the video tag.
+				</video>
+			</div>
+		</section>
+		<div class="h-24" id="space" />
+
 		<!-- ABOUT US -->
-		<section id="about-us" class="h-4/5 py-4 px-4">
-			<h2 class="text-center text-4xl text-gray-300 mb-8">About Us</h2>
+		<section id="about-us" class="flex flex-col items-center">
+			<h2 class="text-center text-4xl text-gray-300 mb-8 w-full">About Us</h2>
 
 			<div
-				class="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-96 p-4 h-full md:h-96 bg-neutral-800 text-neutral-300 rounded-md"
+				class="max-w-sm sm:max-w-md md:max-w-lg lg:max-w-xl p-4 h-full md:h-80 sm:h-60 bg-neutral-800 text-neutral-300 rounded-md"
 			>
 				<h1 class="text-3xl font-bold text-white mb-6">Mission</h1>
 				<div class="text-white ml-4 mr-4 leading-relaxed">
@@ -126,13 +144,12 @@
 					your thoughts, ensuring your data remains anchored and your insights razor-sharp.
 				</div>
 			</div>
-		</section>
-		<div class="h-24 w-full" />
 
-		<!-- How it Works Section -->
-		<section class="mb-24">
+			<div class="h-24 w-full" />
+			<!-- Space between the sections -->
+
 			<div
-				class="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-96 p-4 h-full md:h-96 bg-neutral-800 text-neutral-300 rounded-md"
+				class="max-w-sm sm:max-w-md md:max-w-lg lg:max-w-xl p-4 h-full md:h-80 sm:h-60 bg-neutral-800 text-neutral-300 rounded-md"
 			>
 				<h1 class="text-3xl font-bold text-white mb-6">How it Works</h1>
 				<div class="text-white ml-4 mr-4 leading-relaxed">
@@ -145,14 +162,19 @@
 			</div>
 		</section>
 
-		<!-- Video Section -->
-		<section class="mb-24">
-			<div class="flex justify-center items-center rounded-lg overflow-hidden">
-				<video autoplay loop muted class="max-w-full h-auto">
-					<source src="demoMP4.mp4" type="video/mp4" />
-					Your browser does not support the video tag.
-				</video>
-			</div>
+		<div class="h-24 w-full" />
+		<!-- CTA Section -->
+		<section id="JoinUs" class="h-4/5 py-4 px-4 mt-24 text-center">
+			<h2 class="text-2xl font-bold text-white mb-6">Ready to Dive In?</h2>
+			<p class="text-white mb-6">Discover the difference. Join our community today.</p>
+			<button
+				class="text-lg h-14 px-8 border-neutral-300 text-neutral-300 hover:text-neutral-50 hover:border-neutral-50 hover:bg-neutral-800 font-maven shadow-md"
+				on:click={() => {
+					goto('/dashboard');
+				}}
+			>
+				Get Started
+			</button>
 		</section>
 	</div>
 
