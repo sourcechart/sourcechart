2cdaa6a5d6606c7afe6c8568961c4c89becf8e67
tcollect <tccoleman26@gmail.com>
2023-09-24 12:39:48 -0400

    exporting to csv works

--- src/lib/components/dashboard/sidebar/sidebar-components/ExportToCSV.svelte
+++ src/lib/components/dashboard/sidebar/sidebar-components/ExportToCSV.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
 	import { clickedChartIndex, allCharts, duckDBInstanceStore } from '$lib/io/Stores';
-	import ArrowDown from '$lib/components/ui/icons/ArrowDown.svelte';
 	import { DataIO } from '$lib/io/DataIO';
 
 	$: i = clickedChartIndex();
@@ -14,21 +13,6 @@
 </script>
 
 <button
-	class="selectFieldColor block w-full selectFieldColor text-left px-3 py-2 dark:text-black hover:bg-gray-200"
+	class="selectFieldColor block selectFieldColor text-left px-3 py-2 dark:text-black hover:bg-gray-200"
 	on:click={exportToCSV}
->
-	<div class="flex justify-between items-center w-full">
-		<span>Export to CSV</span>
-		<ArrowDown />
-	</div>
-</button>
-
-<style>
-	.selectFieldColor {
-		background-color: #cd9f34;
-	}
-
-	.selectFieldColor:hover {
-		background-color: #fccb59;
-	}
-</style>
+/>
