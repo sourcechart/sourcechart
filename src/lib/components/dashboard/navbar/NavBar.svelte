73b6c5b5a04c8f4a70887ead08d652875ab68037
tcollect <tccoleman26@gmail.com>
2023-10-02 14:54:26 -0400

    navbar

--- src/lib/components/dashboard/navbar/NavBar.svelte
+++ src/lib/components/dashboard/navbar/NavBar.svelte
@@ -85,8 +85,8 @@
 			<div
 				class={`flex items-center  justify-center mx-1 rounded-md overflow-hidden ${
 					index === activeIndex
-						? 'bg-[#908bd971] hover:bg-[#908bd971]'
-						: 'hover:text-neutral-700/90'
+						? 'bg-[#908bd971]'
+						: 'hover:text-neutral-700/90 hover:bg-neutral-500'
 				}`}
 				on:keypress={(event) => {
 					if (['0', '1', '2', '3'].includes(event.key)) {
