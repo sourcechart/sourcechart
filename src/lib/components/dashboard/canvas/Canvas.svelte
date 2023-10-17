6856788316e657d2c811ae3c9346d9270ec64744
tc-cole <tccoleman26@gmail.com>
2023-10-16 23:58:16 -0400

    Canvas .svetle (#120)

--- src/lib/components/dashboard/canvas/Canvas.svelte
+++ src/lib/components/dashboard/canvas/Canvas.svelte
@@ -223,14 +223,23 @@
 			e.touches.length === 2 &&
 			initialDistance !== null
 		) {
-			const currentDistance = getDistance(e.touches[0], e.touches[1]);
-			const scaleFactor = currentDistance / initialDistance;
+			{
+				const currentDistance = getDistance(e.touches[0], e.touches[1]);
+				let scaleFactor = currentDistance / initialDistance;
 
-			const currentScale = get(scale);
-			scale.set(currentScale * scaleFactor);
+				const currentScale = get(scale); // Assuming 'get' retrieves the current scale value.
+				const newScale = Math.max(0.1, currentScale * scaleFactor); // Ensure the scale doesn't go below 0.1
 
-			initialDistance = currentDistance;
-			return; // We return early after handling zooming.
+				scale.set(newScale);
+
+				initialDistance = currentDistance;
+				polygons.update((polys) => {
+					return polys.map((poly) => {
+						return PolyOps.scaleRectangle(poly, $scale);
+					});
+				});
+				return; // We return early after handling zooming.
+			}
 		}
 
 		if (window.TouchEvent && e instanceof TouchEvent) {
