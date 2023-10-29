9adfdd8efba9c1647f67d1eb29ccf539b9027244
tcollect <tccoleman26@gmail.com>
2023-10-28 23:52:53 -0400

    updates layer components

--- src/lib/components/dashboard/sidebar/sidebar-components/layer-components/H3Layer.svelte
+++ src/lib/components/dashboard/sidebar/sidebar-components/layer-components/H3Layer.svelte
@@ -22,27 +22,48 @@
 
 	$: {
 		if ($allCharts && $allCharts.length > $i && $allCharts[$i]) {
-			allCharts.update((currentCharts) => {
-				let updatedCharts = [...currentCharts];
+			const newLayer: H3HexagonLayer = {
+				type: 'H3HexagonLayer',
+				wireframe: wireframe,
+				pickable: pickable,
+				elevationScale: elevationScale,
+				filled: filled,
+				countColumn: countColumn,
+				extruded: extruded,
+				hexColumn: hexColumn,
+				fillColor: [255, (1 - 1 / 500) * 255, 0]
+			};
 
-				const layer: H3HexagonLayer = {
-					type: 'H3HexagonLayer',
-					wireframe: wireframe,
-					pickable: pickable,
-					elevationScale: elevationScale,
-					filled: filled,
-					countColumn: countColumn,
-					extruded: extruded,
-					hexColumn: hexColumn,
-					fillColor: [255, (1 - 1 / 500) * 255, 0]
-				};
-				updatedCharts[$i].layers[0] = {
-					layerID: generateID(),
-					type: layer
-				};
-				return updatedCharts;
-			});
+			if (!deepEqual($allCharts[$i].layers[0].type, newLayer)) {
+				allCharts.update((currentCharts) => {
+					let updatedCharts = [...currentCharts];
+					updatedCharts[$i].layers[0] = {
+						layerID: generateID(),
+						type: newLayer
+					};
+					return updatedCharts;
+				});
+			}
 		}
+	}
+
+	function deepEqual(obj1: any, obj2: any) {
+		if (obj1 === obj2) return true;
+
+		if (typeof obj1 !== 'object' || obj1 === null || typeof obj2 !== 'object' || obj2 === null) {
+			return false;
+		}
+
+		let keys1 = Object.keys(obj1);
+		let keys2 = Object.keys(obj2);
+
+		if (keys1.length !== keys2.length) return false;
+
+		for (let key of keys1) {
+			if (!keys2.includes(key) || !deepEqual(obj1[key], obj2[key])) return false;
+		}
+
+		return true;
 	}
 
 	async function* transformRows(rows: AsyncIterable<any>) {
@@ -55,8 +76,8 @@
 			data.push(obj);
 
 			if (data.length >= CHUNK_SIZE) {
-				yield data; // Yield the chunk when it reaches the CHUNK_SIZE
-				data = []; // Reset the data list for the next chunk
+				yield data;
+				data = [];
 			}
 		}
 
